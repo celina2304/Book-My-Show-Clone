f3db9eab9d1c7600039c321faaa79eee082c1baa
celina2304 <celina23042001@gmail.com>
2024-12-27 06:11:37 +0530

    testing

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 import bcrypt, { genSaltSync } from "bcrypt";
 import jwt from "jsonwebtoken";
 
--- api/index.js
+++ api/index.js
@@ -35,3 +35,5 @@ app.use('/api/user', userRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+export default app;
